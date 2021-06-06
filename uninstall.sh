0223edb542024f58503991af879ecb4e790727e3
Naowal Rahman <naowal.ar@gmail.com>
2021-06-05 21:45:57 -0400

    added uninstall script

---
+++ uninstall.sh
@@ -0,0 +1,14 @@
+#!/bin/sh
+
+sleep 1
+echo "removing calc from PATH..."
+sudo rm /usr/bin/calc
+
+echo "" 
+ 
+sleep 1 
+echo "removing calc-update from PATH..."
+
+sleep 1
+echo "" 
+echo "Finished uninstallation of calc."
